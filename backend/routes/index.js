e6f1c40dcc78bdc39be21e7cc2566e698c1d4590
Dmitriev-Dmitrii <56238391+dmitriev-dmitrii@users.noreply.github.com>
2022-06-20 17:17:46 +0300

    portSettings

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 
-module.exports = router.get('/', function(req, res, next) {
-  res.send(200, 'test');
+module.exports = router.get('/', function (req, res) {
+  res.send(200, ' res.send ');
 });;
